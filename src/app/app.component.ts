fd86fddf0bc9f832951e4402ce3c2f9f5cf3a88b
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2019-11-18 11:34:07 -0500

    stackblitz does not like inline templates?

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <h1>My Storybook App</h1>
-    <button>What is UP?!</button>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
 export class AppComponent {
