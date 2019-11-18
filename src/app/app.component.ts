a60277dce09ff144cd5b81460f8393311c68ae8e
Kyle Bradshaw <kyle.bradshaw@digitas.com>
2019-11-18 11:03:48 -0500

    fix??

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  // templateUrl: './app.component.html',
   template: `
     <h1>My Storybook App</h1>
     <button>What is UP?!</button>
